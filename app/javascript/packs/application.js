ec69d3b17a5051d1051ce30b02109246f8a6f8e0
Enrique Ramirez <nrqrmz@gmail.com>
2021-09-30 17:20:49 -0500

    Update files

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,13 +2,10 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -16,19 +13,73 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
+// Master DOManipulator v2 ------------------------------------------------------------
+const items = document.querySelectorAll('.item'),
+    controls = document.querySelectorAll('.control'),
+    headerItems = document.querySelectorAll('.item-header'),
+    descriptionItems = document.querySelectorAll('.item-description'),
+    activeDelay = .76,
+    interval = 5000;
 
-// ----------------------------------------------------
-// Note(lewagon): ABOVE IS RAILS DEFAULT CONFIGURATION
-// WRITE YOUR OWN JS STARTING FROM HERE 👇
-// ----------------------------------------------------
+let current = 0;
 
-// External imports
-import "bootstrap";
+const slider = {
+    init: () => {
+        controls.forEach(control => control.addEventListener('click', (e) => { slider.clickedControl(e) }));
+        controls[current].classList.add('active');
+        items[current].classList.add('active');
+    },
+    nextSlide: () => { // Increment current slide and add active class
+        slider.reset();
+        if (current === items.length - 1) current = -1; // Check if current slide is last in array
+        current++;
+        controls[current].classList.add('active');
+        items[current].classList.add('active');
+        slider.transitionDelay(headerItems);
+        slider.transitionDelay(descriptionItems);
+    },
+    clickedControl: (e) => { // Add active class to clicked control and corresponding slide
+        slider.reset();
+        clearInterval(intervalF);
 
-// Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
+        const control = e.target,
+            dataIndex = Number(control.dataset.index);
 
-document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
-});
+        control.classList.add('active');
+        items.forEach((item, index) => {
+            if (index === dataIndex) { // Add active class to corresponding slide
+                item.classList.add('active');
+            }
+        })
+        current = dataIndex; // Update current slide
+        slider.transitionDelay(headerItems);
+        slider.transitionDelay(descriptionItems);
+        intervalF = setInterval(slider.nextSlide, interval); // Fire that bad boi back up
+    },
+    reset: () => { // Remove active classes
+        items.forEach(item => item.classList.remove('active'));
+        controls.forEach(control => control.classList.remove('active'));
+    },
+    transitionDelay: (items) => { // Set incrementing css transition-delay for .item-header & .item-description, .vertical-part, b elements
+        let seconds;
+
+        items.forEach(item => {
+            const children = item.childNodes; // .vertical-part(s)
+            let count = 1,
+                delay;
+
+            item.classList.value === 'item-header' ? seconds = .015 : seconds = .007;
+
+            children.forEach(child => { // iterate through .vertical-part(s) and style b element
+                if (child.classList) {
+                    item.parentNode.classList.contains('active') ? delay = count * seconds + activeDelay : delay = count * seconds;
+                    child.firstElementChild.style.transitionDelay = `${delay}s`; // b element
+                    count++;
+                }
+            })
+        })
+    },
+}
+
+let intervalF = setInterval(slider.nextSlide, interval);
+slider.init(); 
