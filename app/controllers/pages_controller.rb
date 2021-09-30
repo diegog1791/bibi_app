abea5367710e67d71143d232dcd8e752a8d84813
Enrique Ramirez <nrqrmz@gmail.com>
2021-09-30 16:04:23 -0500

    Update acerca, aviso de privacidad, arrendatario, arrendador, adding routes and links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,24 +1,21 @@
 class PagesController < ApplicationController
   skip_before_action :configure_permitted_parameters, only: :home
 
-  def home
-  end
+  def home; end
 
-  def about
-  end
+  def acerca; end
 
-  def garantia
-  end
+  def garantia; end
 
-  def aviso_de_privacidad
-  end
+  def aviso_de_privacidad; end
 
-  def preguntas_frecuentes
-  end
+  def preguntas_frecuentes; end
 
-  def terminos_de_servicio
-  end
+  def terminos_de_servicio; end
 
-  def careers
-  end
+  def careers; end
+
+  def arrendador; end
+
+  def arrendatario; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # pages routes
-  get 'about' => 'pages#about'
+  get 'acerca' => 'pages#acerca'
   get 'garantia' => 'pages#garantia'
   get 'aviso_de_privacidad' => 'pages#aviso_de_privacidad'
   get 'preguntas_frecuentes' => 'pages#preguntas_frecuentes'
   get 'terminos_de_servicio' => 'pages#terminos_de_servicio'
   get 'careers' => 'pages#careers'
+  get 'arrendador' => 'pages#arrendador'
+  get 'arrendatario' => 'pages#arrendatario'
 
   # product routes
   # resources :products
