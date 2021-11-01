412d2dbce6d7bc36eb71b8ce185cbb1c8eaac37e
Enrique Ramirez <nrqrmz@gmail.com>
2021-11-01 15:28:11 -0600

    Add category column and update simple form for

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_01_102921) do
+ActiveRecord::Schema.define(version: 2021_11_01_211812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_10_01_102921) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "category"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
