65393ee907f6e1f9013bb5a974161436a4b782d5
diego garza <diego.garza17@gmail.com>
2021-12-09 18:52:03 -0600

    Sign Up v1.0 Google API Missing, we need to connect it

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,8 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'dfy5vakwb'
+  config.api_key = '132378123232645'
+  config.api_secret = 'rpiwrr_woLdXsLh0S7vdK1l9jvM'
+  config.secure = true
+  
+end
+
