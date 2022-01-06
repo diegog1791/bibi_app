ce0cc1fca3e4efab3277aa1c91c3ca3b87ab396a
diego garza <diego.garza17@gmail.com>
2022-01-06 15:46:25 -0600

    Perfil v1.0 responsiva

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -47,15 +47,15 @@ class PagesController < ApplicationController
   ########### Pefil Methods ###########
 
   def nombre
-    current_user.name || "Aún no nos has dicho tu nombre."
+    current_user.name || "Nombre."
   end
 
   def apellido_paterno
-    current_user.last_name || "Aún no nos has dicho tu apellido paterno."
+    current_user.last_name || "Apellido paterno."
   end
 
   def apellido_materno
-    current_user.mothers_last_name || "Aún no nos has dicho tu apellido materno."
+    current_user.mothers_last_name || "Apellido materno."
   end
 
   def mes_union_bibi
@@ -63,7 +63,7 @@ class PagesController < ApplicationController
   end
 
   def fecha_de_union_a_bibi
-    current_user.created_at.strftime("Te uniste el %d de #{written_month(mes_union_bibi)} de %Y")
+    current_user.created_at.strftime("#{written_month(mes_union_bibi)}/%Y")
   end
 
   def mes_nacimiento
@@ -83,19 +83,19 @@ class PagesController < ApplicationController
   end
 
   def direccion
-    current_user.drop_off_location || "Aún no has agregado tu dirección, agrégala para poder rentar productos."
+    current_user.drop_off_location || "Agregar calle y número."
   end
 
   def colonia
-    current_user.neighborhood || "Aún no has agregado tu colonia. Completa tu información para poder rentar productos."
+    current_user.neighborhood || "Agrega tu colonia."
   end
 
   def codigo_postal
-    current_user.zip_code || "Aún no has agregado tu código postal. Agrégalo para poder brindarte resultados según tu ubicación."
+    current_user.zip_code || "Agrega tu código postal."
   end
 
   def estado
-    current_user.state || "Aún no has agreado tu estado. Agrégalo para poder brindarte resultados según tu ubicación."
+    current_user.state || "Ingresa tu estado."
   end
 
   def direccion_completa
@@ -103,24 +103,24 @@ class PagesController < ApplicationController
   end
 
   def acerca_de_mi
-    current_user.about || "Aquí puedes agregar información acerca de tí, para que otros usuarios puedan conocer mejor tus necesidades y/o servicios."
+    current_user.about || "Aquí puedes agregar información acerca de tí."
   end
 
   def written_month(number)
     month = nil
     case number
-    when 1 then month = "Enero"
-    when 2 then month = "Febrero"
-    when 3 then month = "Marzo"
-    when 4 then month = "Abril"
-    when 5 then month = "Mayo"
-    when 6 then month = "Junio"
-    when 7 then month = "Julio"
-    when 8 then month = "Agosto"
-    when 9 then month = "Septiembre"
-    when 10 then month = "Octubre"
-    when 11 then month = "Noviembre"
-    when 12 then month = "Diciembre"
+    when 1 then month = "Ene"
+    when 2 then month = "Feb"
+    when 3 then month = "Mar"
+    when 4 then month = "Abr"
+    when 5 then month = "May"
+    when 6 then month = "Jun"
+    when 7 then month = "Jul"
+    when 8 then month = "Ago"
+    when 9 then month = "Sep"
+    when 10 then month = "Oct"
+    when 11 then month = "Nov"
+    when 12 then month = "Dic"
     end
     month
   end
